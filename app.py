50e0c4b43e6f96c899795917bfb4081166756d85
Ville-More <73224970+Ville-More@users.noreply.github.com>
2021-07-20 14:15:55 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pickle
+from flask import Flask, request, jsonify, render_template
+
+# Create a Flask application
+
+app = Flask(__name__, template_folder='templates')
+
+# Load pickle model
+
+model = pickle.load(open('Final_MultiOutputRF_model.pkl', 'rb'))
+
+# Create a home page
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Create a POST method
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    
+    return render_template('index.html', prediction_text = 'EC & pH should be {}'.format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
